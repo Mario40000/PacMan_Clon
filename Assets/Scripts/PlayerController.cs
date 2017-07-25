72b16c2e5cf70d81e36d9f58a6fd25ab072d637b
Mario40000 <mario40000@hotmail.com>
2017-07-25 18:34:27 +0200

    Boceto del mapa y movimiento del player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+		
+	}
+
+    //Desde aqui controlamos el movimiento del jugador
+    void FixedUpdate ()
+    {
+        //Variables para recoger la posicion de los ejes
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Actualizamos la posicion de la nave multiplicando movimiento por velocidad
+        Vector3 movement = new Vector3((moveHorizontal) , 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movement * playerSpeed;
+    }
+}
