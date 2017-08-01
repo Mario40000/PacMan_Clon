ec86db6f68f66ca8529ac6391a0cc8f97206ff52
Mario40000 <mario40000@hotmail.com>
2017-08-01 20:18:00 +0200

    Metodo para comer bolas

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticData
+{
+    //variables estaticas
+    public static int score = 0;
+    public static int hiScore = 0;
+    public static int lives = 3;
+
+	
+}
